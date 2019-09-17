db230b0f416fb815e44367dd065ea3119c9f8677
zrpalladino4985 <54415489+zrpalladino4985@users.noreply.github.com>
2019-09-17 14:02:00 -0400

    Add files via upload

---
+++ Quiz Game.py
@@ -0,0 +1,24 @@
+#Zachary Palladino
+#This program is a quiz game that gives the user multiple choice questions
+
+print("Hello! Welcome to my quiz game! \nI am going to give you different multiple choice questions and you have to answer them correctly")
+print(" ")
+print("Question 1")
+
+a=input("What is the derivative of the function: 2x+5? ")
+
+print("If your answer was 2, then you are correct!")
+        
+print("Question 2")
+
+b=input("What date was the Declaration of Independence signed? ")
+
+print("If your answer was 1776, then you are correct!")
+
+print("Question 3")
+
+c=input("What class is this? ")
+
+print("If your answer was COP 1500, then you are correct!")
+
+print("Nice job! hopefully you did good!")
